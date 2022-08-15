ff46c62bc801723ed05a4574ffb1b03bfddf7fa5
Tshiamo Phaahla <72293519+TPhaahla@users.noreply.github.com>
2022-08-15 18:20:56 +0200

    page nav from header

--- cbib-webapp/components/Header.js
+++ cbib-webapp/components/Header.js
@@ -5,15 +5,16 @@ function Header() {
 
     const router = useRouter()
   return (
-    <div className=" bg-gray-50">
+    <div className="h-14 border-b bg-gray-100">
 
      
 
-        <nav className='text-pink-300'>
-            <ul className="flex justify-around ">
+        <nav className='h-14'>
+            <ul className="flex justify-around items-center h-full">
                 <li onClick={()=>router.push("/")} className="cursor-pointer ">Home</li>
                 <li onClick={()=>router.push("/publication")} className="cursor-pointer">Publication</li>
                 <li onClick={()=>router.push("/dashboard")} className="cursor-pointer">Dashboard</li>
+                <li onClick={()=>router.push("/profile")} className="cursor-pointer">Profile</li>
             </ul>
         </nav>
 

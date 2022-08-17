40d7d4b20280c7fd1129d38eca830f4be20d9a69
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2022-08-17 16:13:08 +0200

    version 2.0

--- cbib-webapp/pages/publication.js
+++ cbib-webapp/pages/publication.js
@@ -4,72 +4,106 @@ import Header from '../components/Header'
 
 export default function Publication(name) {
     
+    // dummy data for profiles 
     const profiles = [
+        {name:"Sphesihle Madonsela",position:"Student",image:"",id:4},
         {name:'Tommie Meyer',position:"Researcher,Co-ordinator",image:"https://randomuser.me/api/portraits/men/90.jpg",id:1},
         {name:'User2 XSms',position:"Researcher, Student",image:"",id:2},
-        {name:"Aser4 JJDJ",position:"Researcher, Student",image:"https://randomuser.me/api/portraits/men/96.jpg",id:3},
-        {name:"Sser3 DS",position:"Admin, Group Admin",image:"https://randomuser.me/api/portraits/men/97.jpg",id:4}
+        //{name:"Aser4 JJDJ",position:"Researcher, Student",image:"https://randomuser.me/api/portraits/men/96.jpg",id:3},
+        //{name:"Sser3 DS",position:"Admin, Group Admin",image:"https://randomuser.me/api/portraits/men/97.jpg",id:4},
     ];
-    return (
 
-        
-        <div>
-            
+    // Dummy data for paper aploaded 
+    const uploads = [
+        'publication1.pdf',
+        'publication1.tex'
+    ];
+
+
+    return ( 
+
+        <div >
             <header>
                 <Header/>
             </header>
-            Publication Page
-
-            <div  className = "columns-3 flex justify-around  text-2xl text-sky-500">
-                <div>
+           
+            
+            <div  className = "flex flex-row gap-2.5 text-2xl text-black max-h-full">
+                <div className='container flex basis-1/4 flex-col  justify-content items-center '>
                     <p >Collaborators</p>
-                    <div class = "container mx-auto  flex flex-row  content-start bg-gray-100 rounded  shadow-xl"> 
-                        
-                        <ul class='pt-5 pb-5 pr-5 pl-5 flex flex-col-reverse space-y-5 space-y-reverse '>
-
-                            <button class="bg-blue-300 hover:bg-gray-400 text-gray-600 font-bold py-1 px-4 rounded inline-flex items-center">
-                              <span class="pl-5">Add/Remove<br/>Collaborators</span>
-                              
-                            </button>
+                    <div class = "flex flex-row  justify-center items-center bg-white-100 rounded shadow-xl mt-4 border border-black"> 
+                        <ul class='p-5 flex flex-col space-y-5  '>
                             {
-                                
-                                profiles.map( (user) => <Card pic={user.image} userName = {user.name} role = {user.position}/>)
-
+                                profiles.map( (user) => <Card pic={user.image} userName = {user.name} role = {user.position} />)
                             }
-
-                            
-                           
+                            <button className="bg-blue-300 hover:bg-gray-400 text-gray-600 font-bold py-1 px-4 rounded inline-flex items-center">
+                              <span className="pl-5">Add/Remove<br/>Collaborators</span>
+                            </button>
                         </ul>
                     </div>
                 </div>
                 
-                <div>Metadata</div>
+                <div className='flex basis-1/2 flex-col bg-white-100 px-5  '>
+                    <p>Metadata</p>
 
-                <div>Uploads</div>
+                    <div className='w-full h-80 border border-gray-500 mt-4 flex justify-center items-center'>   
+                        <p>Block Reserved for associated <br/>publication metadata</p>
+                    </div>
+                    <p className='pt-4'>Reviews</p>
+                    <div className='w-full  border border-gray-500 h-80 mt-2 flex justify-center items-center'>   
+                        <p>Block Reserved for associated <br/>publication metadata</p>  
+                    </div>
+                </div>
+                            
+                <div className='flex flex-col basis-1/4 bg-white-100  border-gray-100 px-4'>
+                    <p>Uploads</p>
+                    
+                    <div className='mt-4 border border-black w-full flex justify-center items-center '>
+                        <ul>
+                                {uploads.map( (file) => <p>{file}</p>) }
+                                <button 
+                                    className='
+                                    bg-blue-300
+                                    text-gray-600 
+                                    items-center
+                                    border border-slate-500
+                                    font-semibold 
+                                    rounded inline-flex 
+                                    hover:bg-gray-300
+                                    mt-5
+                                    mb-5
+                                    '
+                                >
+                                    <span className='px-5'>Upload NewFile</span>
+                                </button>
+                        </ul>
+
+                    </div>
+                </div>
                 
             </div>
         </div>
     )
 }
-
-
-// This takes 
+/**
+ *This is a functional component  
+ *  its props are {pic,userName,role}
+ */
 export  const Card = (props) =>{
 
-    
     return(
-        <div className = "column-2 hover:bg-gray-200 flex flex-row content-between border rounded-lg border-x-2 border-indigo-900 shadow-xgol cursor-pointer "> 
+        <div className = "hover:bg-gray-200 flex flex-row content-between border rounded-lg border-x-2 border-indigo-900 shadow-xl cursor-pointer "> 
            
-            <div class="pt-2 pr-2 pl-2 relative w-20 h-20">
+            <div className="pt-2 pr-2 pl-2 relative w-20 h-20">
                 {/* <img class = "rounded-full boader boader-grey-100 shadow-sm boader shrink-0" src={props.pic} alt=''/> */}
                 {
                     (props.pic.length!==0)?<img class = "rounded-full boader boader-grey-100 shadow-sm boader shrink-0" src={props.pic} alt=''/>:<PlaceHolder userName={props.userName}/>
                 }
             </div>
 
-            <div class="text-sky-900 font-semibold pt-5 ltr:ml-3 rtl:mr-3">
-                <p class="text-sm font-medium text-bule-300 group-hover:text-blue-900">{props.userName}</p>
-                <p class="text-sm font-medium text-blue-500 group-hover:text-slate-300">{props.role}</p>
+            <div className="text-sky-900 font-semibold pt-5 ltr:ml-3 rtl:mr-3">
+                <p className="text-sm font-medium text-slate-900 group-hover:text-slate-900">{props.userName}</p>
+                <p className="text-sm font-medium text-slate-500 pr-1 ">{props.role}</p>
             </div>
 
             {/* <PlaceHolder userName  = {props.userName}/> */}
@@ -83,11 +117,9 @@ export const PlaceHolder = ({userName}) =>{
     const arr = userName.split(' ');
     initials = arr[0][0]+arr[1][0] // getting the initials 
     return(
-        
-        <div className="rounded-full boader boader-grey-900 p-4 border-2 shadow-xl boader shrink-0">
-            
-            <p className=''>{initials}</p>
-            
+
+        <div className="border-2 border-black rounded-full w-14 h-14 flex justify-center items-center">
+            <p className='font-semibold text-2xl text-red-400'>{initials}</p>
         </div>
     );
 }

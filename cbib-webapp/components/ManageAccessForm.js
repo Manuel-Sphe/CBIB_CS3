8c8a685af51da1187ced48610a5fbaf70f0f2127
Tshiamo Phaahla <72293519+TPhaahla@users.noreply.github.com>
2022-09-07 10:42:10 +0200

    Create Form for inviting members to group

---
+++ cbib-webapp/components/ManageAccessForm.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+function InviteMemberForm() {
+  return (
+    <div className="fixed top-0 w-screen h-screen bg-black bg-opacity-30 items-center justify-center align-center h-screen">
+        
+        <div className="relative top-1/4 mx-auto border w-2/5 space-y-5 p-5 rounded-lg bg-white ">
+
+            <h2 className="text-xl flex justify-center"> Add New Member to CAIR  </h2>
+            <div className="flex justify-between items-center">
+                <p className="">First Name: </p>
+                <input type="text" className="border border-black p-5 w-2/3 h-8" placeholder='Jane' />
+            </div>
+
+            <div className="flex justify-between items-center">
+                <p className="">Last Name: </p>
+                <input type="text" className="border border-black p-5 w-2/3 h-8" placeholder='Doe' />
+            </div>
+
+            <div className="flex justify-between items-center">
+                <p className="">Email: </p>
+                <input type="text" className="border border-black p-5 w-2/3 h-8" placeholder='example@cair.org' />
+            </div>
+
+            <div className="flex justify-between items-center">
+                <p className="">Role</p>
+                <select className="w-2/3"> 
+                    <option>Student</option>
+                    <option>Researcher</option>
+                    <option>Group Admin</option>
+                    <option>Group Leader</option>
+                </select>
+            </div>
+
+                    
+
+            <div className="flex justify-between items-center">
+                <p className="">Research Group Code: </p>
+                <div className="w-2/3">
+                    <input type="text" className="border border-black p-5 w-full h-8" placeholder='enter alphanumeric group code...' />
+                    <p className="text-xs text-gray-500"></p>
+                </div>
+            </div>
+
+            <div className="w-full flex justify-center">
+                <button type="button" className="bg-blue-300 p-3 rounded-lg"> Invite New Member </button>
+            </div>
+
+
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default InviteMemberForm;

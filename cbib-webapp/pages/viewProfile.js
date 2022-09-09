25544afbc6556a6548573918b465d80170422985
Tshiamo Phaahla <72293519+TPhaahla@users.noreply.github.com>
2022-09-09 14:55:23 +0200

    edit viewProfile Layout

--- cbib-webapp/pages/viewProfile.js
+++ cbib-webapp/pages/viewProfile.js
@@ -2,6 +2,7 @@ import React, { useContext, useState } from 'react';
 import Header from '../components/Header';
 import {useRouter} from "next/router";
 import  {ProfileDetailsContext, ProfileDetailsProvider}  from '../Global/ProfileDetailsContext';
+import Image from "next/image";
 
 export default function ViewProfile(props) {
     
@@ -16,39 +17,91 @@ export default function ViewProfile(props) {
         <header>
             <Header/>
         </header>
-        
-        <div className='flex flex-row mt-10'>
-            <div className='flex basis-1/4 justify-center'>
-                <div className='flex w-72 h-72 justify-center'>
-                    <img className='rounded' src={'https://media.npr.org/assets/img/2015/10/22/gettyimages-475425810-c4f2d327fc8a7a2f39098dfbae1012cc9040f1d2-s600-c85.webp'}/>
+
+        <main className="space-y-12 mt-8">
+            
+            {/* Profile Summary Section */}
+
+            <section className="flex justify-center items-center space-x-8">
+                <div className="bg-gray-400 w-36 h-36 rounded-full">
+                    {/* <Image src="/templatePic.jpg" layout="fill"/> */}
                 </div>
-            </div>
 
-            <div className='flex basis-1/2 justify-start flex-col h-screen space-y-10'>
-                <h1 className='flex text-5xl ml-20'>
-                    name
-                </h1>
-                <h1 className='flex text-4xl mx-20 '>
-                    university
-                </h1>
-                <h1 className='flex text-4xl mx-20'>
-                    groupName
-                </h1>
-                <h1 className='flex text-4xl mx-20'>
-                    Bio
-                </h1>
-                <h1 className='flex text-xl mx-20'>
-                    bio
-                </h1>
+                <div className="w-2/5 space-y-2">
+                    <h2 className="border-b text-xl font-semibold col-span-2">Basic Information</h2>
+                    <div className="flex justify-between items-center">
+                        <p className="text-sm">Name: </p>
+                        <p className="w-2/3">John Doe</p>
+                    </div>
+
+                    <div className="border-b"/>
+
+
+                    <div className="flex justify-between items-center">
+                        <p className="text-sm">Organisation: </p>
+                        <p className="w-2/3">John Doe</p>
+                    </div>
+                    <div className="border-b"/>
+
+                    <div className="flex justify-between items-center">
+                        <p className="text-sm">Research Interests: </p>
+                        <p className="w-2/3"></p>
+                    </div>
+                    <div className="border-b"/>
+
+
+                    <div className="flex justify-between items-center">
+                        <p className="text-sm">Research Groups: </p>
+                        <p className="w-2/3">Knowledge Representation and Reason (KRR) <br/> Application of Bayesian Networks to ECG Interpretation</p>
+                    </div>
+
+                </div>
+            </section>
+
+
+            {/* Research Papers and Contributions */}
+            <section className=" w-3/5 mx-auto">
+
+                <div className="w-full">
+                    <PubCard publicationName="Whatever You Want"/>
+                    <PubCard publicationName="Whatever You Want"/>
+                    <PubCard publicationName="Whatever You Want"/>
+                    <PubCard publicationName="Whatever You Want"/>
+
+                </div>
                 
-            </div>
 
-            <div className='flex basis-1/4 justify-end'>
+            </section>
+
+
+        </main>
+
+                
+        
+
+
+
+            {/* <div className='flex basis-1/4 justify-end'>
                 <button type='button' onClick={()=>router.push("/profile")} className='flex rounded bg-blue-500  h-fit py-0.5 px-4 hover:bg-blue-600'>
                     Edit profile
                 </button>
-            </div>
-        </div>
+            </div> */}
+       
     </div>
   )
+}
+
+function PubCard({publicationName}) {
+    return (
+        <div className='w-11/12 h-1/6 border text-gray-700 rounded mx-4 my-5'>
+            <h1 className='flex justify-center mt-3 ml-4 text-sm'>
+                {publicationName}
+            </h1>
+            <div className='flex flex-row justify-around my-4'>
+                <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer'>View</h1>
+                <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer'>Edit</h1>
+
+            </div>
+        </div>
+    );
 }

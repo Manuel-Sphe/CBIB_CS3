893262d843eb626122d2eed26b1b0511b706c940
Jose <107567749+zeptr@users.noreply.github.com>
2022-09-12 13:18:48 +0200

    research publication bold text for conference

--- cbib-webapp/pages/researchGroupResearchPublications.js
+++ cbib-webapp/pages/researchGroupResearchPublications.js
@@ -11,7 +11,13 @@ export default function ResearchGroup() {
     const groupAdmin = 'Tshiamo Phaahla';
     const groupCoordinator = 'Prof. Tommie Meyer';
     const researchers = ['Tommie, ', 'Kevaalin, ', 'Sphe, ', 'José, ', 'Tshiamo'];
-    const Publications = ['Hamilton J, Park J, Bailey A, Meyer T. An Investigation into the Scalability of Defeasible Reasoning Algorithms. In: Second Southern African Conference for Artificial Intelligence. Online: SACAIR 2021 Organising Committee; 2022. https://protect-za.mimecast.com/s/OFYSCpgo02fL1l9gtDHUkY.', 'Two Sepedi-English code-switched speech corpora', 'Combining Machine Learning and Bayesian Networks for ECG Interpretation and Explanation']
+    const Publications = [
+        {collaborators: 'Hamilton J, Park J, Bailey A, Meyer T. ', publicationName: 'An Investigation into the Scalability of Defeasible Reasoning Algorithms.', conference: 'In: Second Southern African Conference for Artificial Intelligence.', link: 'Online: SACAIR 2021 Organising Committee; 2022. https://protect-za.mimecast.com/s/OFYSCpgo02fL1l9gtDHUkY.'},
+        {collaborators: 'Baker CK, Meyer T.', publicationName: 'Belief Change in Human Reasoning: An Empirical Investigation on MTurk.', conference: 'In: Second Southern African Conference for AI Research (SACAIR 2022).', link: 'Online: SACAIR 2021 Organising Committee; 2022. https://protect-za.mimecast.com/s/OFYSCpgo02fL1l9gtDHUkY.'},
+        {collaborators: 'Everett L, Morris E, Meyer T.', publicationName: 'Explanation for KLM-Style Defeasible Reasoning.', conference: 'In: Artificial Intelligence Research. SACAIR 2021.', link: '1551st ed. Cham: Springer; 2022. doi:10.1007/978-3-030-95070-5_13.'}
+        
+    ];
+    
     const router = useRouter()
 
     const [showForm, setShowForm] = useState(false);
@@ -24,13 +30,20 @@ export default function ResearchGroup() {
     function PubCard({publicationName}) {
         return (
             <div className='w-11/12 h-1/6 border text-gray-700 rounded mx-4 my-5'>
-                <h1 className='flex mt-3 ml-4 text-sm'>
-                    {publicationName}
-                </h1>
+                    <h1 className='flex mt-3 ml-4 text-sm'>
+                    {publicationName.collaborators}{publicationName.publicationName}
+                        {publicationName.conference}
+                    {publicationName.link}
+                    </h1>
+                    
+
+                
                 <div className='flex flex-row justify-around my-4'>
-                    <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer'>View</h1>
-                    <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer'>Edit</h1>
-                    <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer' onClick={toggleForm}>Manage Access</h1>
+                    <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer'>View Abstract</h1>
+                    <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer'>BibTex Entry</h1>
+                    <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer'>BibTex Download</h1>
+                    <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer'>Download PDF</h1>
+                    <h1 className='text-stone-700 hover:text-sky-400 text-xs cursor-pointer' onClick={toggleForm}>Edit Publication</h1>
 
                 </div>
             </div>

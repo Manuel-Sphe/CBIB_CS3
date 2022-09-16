758ca20b5397de91395bd0ddad91c6183423e2a7
Tshiamo Phaahla <72293519+TPhaahla@users.noreply.github.com>
2022-09-16 10:43:42 +0200

    correct router imports

--- cbib-server/app/main.py
+++ cbib-server/app/main.py
@@ -1,10 +1,11 @@
 from . models import Profile
-
-from .routers import profile, groups, organisation, publication, uploads, users, login, media_uploads
+from fastapi import FastAPI
+from . import database
+from .routers import groups, organisation, publication, uploads, users, login, media_uploads
 from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
-app.include_router(profile.router)
+app.include_router(users.router)
 app.include_router(organisation.router)
 app.include_router(groups.router)
 app.include_router(publication.router)

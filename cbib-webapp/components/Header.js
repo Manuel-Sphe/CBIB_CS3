f5ef51867a105ccd000c4c5fb83c84d00ee97681
Jose <107567749+zeptr@users.noreply.github.com>
2022-09-19 15:35:52 +0200

    dwd

--- cbib-webapp/components/Header.js
+++ cbib-webapp/components/Header.js
@@ -14,17 +14,10 @@ function Header() {
         <nav className='h-14'>
             <ul className="flex justify-around items-center h-full">
                 <li onClick={()=>router.push("/dashboard")} className="cursor-pointer">Dashboard</li>
-<<<<<<< HEAD
                 <li onClick={()=>router.push("/viewProfile")} className="cursor-pointer">View Profile</li>
                 <li onClick={()=>router.push("/profile")} className="cursor-pointer">Edit Profile</li>
                 <li onClick={ ()=>router.push("/viewResearchGroups")} className= "cursor-pointer">Research Groups</li>
                 
-=======
-                <li onClick={()=>router.push("/viewProfile")} className="cursor-pointer">Profile</li>
-                <li onClick={ ()=>router.push("/manageResearchG")} className= "cursor-pointer">Research</li>
-                <li onClick={ ()=>router.push("/createResearchGroup")} className= "cursor-pointer">Create</li>
-                <li onClick={() =>router.push('/manageUsers')}>ManageUsers</li>
->>>>>>> 1d3fb84b32669d6eccef282bfafc782bb761d9c4
             </ul>
         </nav>
 

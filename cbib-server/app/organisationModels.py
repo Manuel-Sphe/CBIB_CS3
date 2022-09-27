7aada04a6032f92ddff7d4d4191fac5d800c49bb
Tshiamo Phaahla <72293519+TPhaahla@users.noreply.github.com>
2022-09-27 15:21:03 +0200

    add auth for pub and group routes

--- cbib-server/app/organisationModels.py
+++ cbib-server/app/organisationModels.py
@@ -65,7 +65,8 @@ class Publication(BaseModel):
     abstract: str = Field(...)
     authors: List[str]
     owner_group: str
-    content: str
+    collaborators: List[str] # ID's of all CAIR Member Contributors
+    # content: str
     date_created: datetime
     last_modified: datetime = Field(default_factory=datetime.now())
     
@@ -74,11 +75,20 @@ class Publication(BaseModel):
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         schema_extra = {
+            "example":{
+                "title":"Lorem ipsum dolor sit amet.",
+                "abstract":"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores magni nam beatae officiis minus ipsam, delectus atque facilis, nesciunt rem modi ipsum, velit voluptate molestiae architecto odio debitis quaerat exercitationem doloremque voluptatibus necessitatibus alias! Expedita voluptatibus pariatur ex perspiciatis eaque. Dolor possimus tempore distinctio quibusdam ipsam. Assumenda hic aperiam quibusdam?",
+                "authors":["Tshiamo, Sphe, Jose"],
+                "owner_group":"6332df06ca6ed4ee1a537192",
+                "collaborators":["6332f4189a3803293a3f3047"],
+                "date_created":datetime.now(),
+                "last_modified":datetime.now()
+            }
 
         }
 
 class Access(BaseModel):
-    group: str #group id of the group 
+    level: str #user level
     roles: List[str] #store code of their role i.e Super Admin, Student, Researcher, Viewer
 
     class Config:
@@ -93,8 +103,7 @@ class Access(BaseModel):
 
 class Member(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    user: str #user Id of this member profile
-    groups: List[Access] #Store member group access level info
+    
 
 
     class Config:
@@ -105,13 +114,28 @@ class Member(BaseModel):
 
         }
 
+class AddMember(BaseException):
+    access_group: str
+    member_id: List[str]
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        # json_encoders = {ObjectId: str}
+        schema_extra = {
+            "access_group":"student_researchers",
+            "member_id":["6332a5fb7b21e1d99952a1d4"]
+
+        }
 
 class ResearchGroup(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     group_name: str = Field(...)
     organisation: str = Field(...) #Store the Parent Organisation ID here.
+    description: str = Field(...)
     # publications: List[Publication] # Store each publication that this group contributed 
-    members: List[Member]
+    # admin()
+    members: dict
 
     class Config:
         allow_population_by_field_name = True
@@ -120,14 +144,23 @@ class ResearchGroup(BaseModel):
         schema_extra = {
             "example":{
                 "group_name":"KKP",
-                "organisation":"6321d7a5e85cee8f30fd719a",
-                "members":[]
+                "organisation":"6332da5b82bd851520ac3fb7",
+                "description":"some fancy words here",
+                "members": {
+                    "group_coordinator": [],
+                    "group_leaders":[],
+                    "research_admins":[],
+                    "researchers":[],
+                    "student_researchers":[]
+	
+                }
+
             }
         }
 
 class UpdateResearchGroup(BaseModel):
     group_name: Optional[str]
-    organisation: Optional[str]  #Store the Parent Organisation ID here.
+    description: Optional[str]  #Store the Parent Organisation ID here.
     # publications: List[Publication] # Store each publication that this group contributed 
     # members: List[Member]
     
@@ -139,27 +172,29 @@ class UpdateResearchGroup(BaseModel):
         schema_extra = {
             "example":{
                 "group_name":"KKP",
-                "organisation":"6321d7a5e85cee8f30fd719a"            }
+                "description":"We just try and always try"
+                }
         }
-class ResearchGroup(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    group_name: str = Field(...)
-    organisation: str = Field(...) #Store the Parent Organisation ID here.
-    publications: List[Publication] # Store each publication that this group contributed 
-    members: List[Member]
 
-    class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example":{
-                "group_name":"KKP",
-                "organisation":"6321d7a5e85cee8f30fd719a",
-                "publications":[],
-                "members":[]
-            }
-        }
+# class ResearchGroup(BaseModel):
+#     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+#     group_name: str = Field(...)
+#     organisation: str = Field(...) #Store the Parent Organisation ID here.
+#     publications: List[Publication] # Store each publication that this group contributed 
+#     members: List[Member]
+
+#     class Config:
+#         allow_population_by_field_name = True
+#         arbitrary_types_allowed = True
+#         json_encoders = {ObjectId: str}
+#         schema_extra = {
+#             "example":{
+#                 "group_name":"KKP",
+#                 "organisation":"6321d7a5e85cee8f30fd719a",
+#                 "publications":[],
+#                 "members":[]
+#             }
+#         }
 
 # class MongoBase(BaseModel):
 #     id: Optional[PyObjectId] = Field(alias="_id")
--- cbib-server/app/routers/groups.py
+++ cbib-server/app/routers/groups.py
@@ -28,7 +28,9 @@ async def create_group(group: orgModel.ResearchGroup, current_user: str = Depend
     org_admins.extend(organisation["super_admins"])
 
     if current_user in org_admins:
-
+        group["admins"] = [current_user]
+        group["super_admins"] = [current_user]
+        print(group)
         new_group = await db["research_groups"].insert_one(group)
         ## Add code here to update the groups attribute of the parent organisation
         created_group = await db["research_groups"].find_one({"_id":new_group.inserted_id})
@@ -44,36 +46,98 @@ async def get_group_by_id(id:str):
     group = await db["research_groups"].find_one({"_id":id})
     return group
 
+## Get All Research Groups
+@router.get("/")
+async def get_all_groups():
+    group = await db["research_groups"].find().to_list(1000)
+    return group
+
+## Add/Remove Member from Research Group (include date they were added and past member data)
+@router.put("/manage_members/{action}/{groud_id}")
+async def manage_members(action: str, group_id: str,members:dict, current_user: str = Depends(oauth2.get_current_user)):
+    
+    # members = jsonable_encoder(members)
+
+    group = await get_group_by_id(group_id)
+    group = jsonable_encoder(group)
+    organisation = await get_organisation(group["organisation"])
+    organisation = jsonable_encoder(organisation)
+
+    group_admins = group["admins"]
+    group_admins.extend(organisation["super_admins"])
+    group_admins.extend(organisation["admins"])
+    group_admins.extend(group["super_admins"])
+
+    if current_user in group_admins :
+        return {
+            "message": "THIS METHOD IS INCOMPLETE"
+        }
+        
+
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
+
+
 
 ## Update Research Group by ID 6321d810ee5ddca14bf15a9d
 @router.put("/{id}")
-async def update_research_group(id:str, research_group: orgModel.UpdateResearchGroup ):
+async def update_research_group(id:str, research_group: orgModel.UpdateResearchGroup, current_user: str = Depends(oauth2.get_current_user) ):
+
+    group = await get_group_by_id(id)
+    group = jsonable_encoder(group)
+    organisation = await get_organisation(group["organisation"])
+    organisation = jsonable_encoder(organisation)
+
+    group_admins = group["admins"]
+    group_admins.extend(organisation["super_admins"])
+    group_admins.extend(organisation["admins"])
+    group_admins.extend(group["super_admins"])
+
+    if current_user in group_admins :
     
-    group = {k: v for k, v in research_group.dict().items() if v is not None}
-    print(group)
-    if len(group) >= 1:
-        update_result= await db["research_groups"].update_one({"_id":id}, {"$set":group})
+        group = {k: v for k, v in research_group.dict().items() if v is not None}
+        print(group)
+        if len(group) >= 1:
+            update_result= await db["research_groups"].update_one({"_id":id}, {"$set":group})
 
-        if update_result.modified_count == 1:
-            if (
-                updated_group := await db["research_groups"].find_one({"_id": id})
-            ) is not None:
-                return updated_group
-                # print(updated_group)
+            if update_result.modified_count == 1:
+                if (
+                    updated_group := await db["research_groups"].find_one({"_id": id})
+                ) is not None:
+                    return updated_group
+                    # print(updated_group)
 
-    if (existing_group := await db["research_groups"].find_one({"_id": id})) is not None:
-        return existing_group
+        if (existing_group := await db["research_groups"].find_one({"_id": id})) is not None:
+            return existing_group
+    
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
         # print(existing_group)
 
 ## Delete a Research Group by ID 
 @router.delete("/{id}")
-async def delete_group(id:str):
+async def delete_group(id:str, current_user = Depends(oauth2.get_current_user)):
 
-    delete_result = await db["research_group"].delete_one({"_id":id})
+    group_current = await get_group_by_id(id)
+    group_current = jsonable_encoder(group_current)
+    admins = group_current["admins"]
+    admins.extend(group_current["super_admins"])
+    org_current = await get_organisation(group_current["organisation"])
+    org_current = jsonable_encoder(org_current)
+    admins.extend(org_current["admins"])
+    admins.extend(org_current["super_admins"])
+    # print(org)
+    if current_user in admins:
 
-    if delete_result.deleted_count==1:
-        return {
-            "message":"File Deleted"
-        }
+        delete_result = await db["research_group"].delete_one({"_id":id})
 
-    raise HTTPException(status_code=404, detail=f"Group with id of {id} not found")
+        if delete_result.deleted_count==1:
+            return {
+                "message":"File Deleted"
+            }
+
+        raise HTTPException(status_code=404, detail=f"Group with id of {id} not found")
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
